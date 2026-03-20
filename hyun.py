98edc01badd12a89e2f02370772f2815e1b11426
huybeohub <nguyengiahuybe012@gmail.com>
2026-03-20 22:03:30 +0700

    Update hyun.py

--- hyun.py
+++ hyun.py
@@ -103,4 +103,10 @@ def home():
     return "API OK"
 
 
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 3000))
+    app.run(host="0.0.0.0", port=port)
+
 # ❌ KHÔNG DÙNG app.run() KHI DEPLOY RENDER
